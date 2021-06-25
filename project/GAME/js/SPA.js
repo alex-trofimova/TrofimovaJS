084d159588fa53a50829c1cb896ebf9db17e62ad
alex-trofimova <alex.trofimova@gmail.com>
2021-06-25 23:49:29 +0300

    changes2

--- project/GAME/js/SPA.js
+++ project/GAME/js/SPA.js
@@ -25,13 +25,14 @@ contGame.width = document.body.clientWidth;
 function resizeGameField() {
     contGame.width = document.body.clientWidth;
     console.log(contGame.width);
-    var svgCont = document.getElementById('svgcont');
-    if (contGame.width<340) {
+    
+    if (contGame.width<540) {
+        var svgCont = document.getElementById('svgcont');
         console.log('ширина стала меньше 540');
         svgCont.setAttribute('viewBox', '40 0 760 700');
     }
     else {
-
+        var svgCont = document.getElementById('svgcont');
         svgCont.setAttribute('viewBox', '0 0 1200 480');
     }
 }
